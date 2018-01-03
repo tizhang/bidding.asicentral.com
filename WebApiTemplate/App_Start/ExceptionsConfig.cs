1a105c8c2bdc39a6a78a493b37e375d076c80576
Max Stepanskiy <mstepanskiy@asicentral.com>
2018-01-03 15:06:27 -0500

    Updated HTTP library

--- WebApiTemplate/App_Start/ExceptionsConfig.cs
+++ WebApiTemplate/App_Start/ExceptionsConfig.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
 using ExceptionHandler = ASI.Services.Http.Exceptions.ExceptionHandler;
 
 namespace WebApiTemplate
@@ -14,9 +18,11 @@ namespace WebApiTemplate
         {
             bool.TryParse(ConfigurationManager.AppSettings["Errors:IncludeStackTrace"], out bool includeStackTrace);
 
+            bool.TryParse(ConfigurationManager.AppSettings["Errors:CaptureBaseException"], out bool captureBaseException);
+
             var logger = new Services.ExceptionLogger();
 
-            var handler = new ExceptionHandler(logger, includeStackTrace);
+            var handler = new ExceptionHandler(logger, includeStackTrace, captureBaseException);
 
             handler.Register<ApplicationException>(HttpStatusCode.BadRequest)
                 .Register<FileNotFoundException>(HttpStatusCode.NotFound);
