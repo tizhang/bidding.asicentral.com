da45237842e2add0829558f6e51d2da7deaac7df
mzhang-asi <mzhang@asicentral.com>
2018-01-23 14:23:39 -0500

    Add User and Watcher tables

---
+++ Bidding.Data/Watcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bidding.Data
+{
+    public class Watcher
+    {
+        public int WatcherId { get; set; }
+        public long BiddingItemId { get; set; }
+        public int UserId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual BiddingItem BiddingItem { get; set; }
+    }
+}
