fa3367da514e810437ddfe28e562dc2a8a996c81
Caroline Chen <cchen@asicentral.com>
2018-01-24 18:30:46 -0500

    Notification with mock data

--- Bidding.Web/App/modules/bidding/controllers/MenuController.js
+++ Bidding.Web/App/modules/bidding/controllers/MenuController.js
@@ -17,19 +17,31 @@
       { Code: 'MyHistory', Text: 'My History', State: 'bidding.history', UpdatesCount: 0},
       { Code: 'Watch', Text: 'Watch', State: 'watch', UpdatesCount: 0}
     ];
+    vm.AlertList = [];
 
     vm.gotoTab = gotoTab;
+    vm.viewItem = viewItem;
     
     init();
 
     function init() {
       vm.currentTab = angular.copy(vm.tabs[0]);
+
+      vm.AlertList = [
+        { Id: 1, BiddingItemId: 1, ImageUrl: '', Message: 'bidder tbidding1 submitted new bid price 2 for pen', CreateDate: '2018-01-24T09:43:43.313', EventTime: '2018-01-24T09:43:43.313'},
+        { Id: 2, BiddingItemId: 1, ImageUrl: '', Message: 'bidder tbidding2 submitted new bid price 16 for pen', CreateDate: '2018-01-24T09:43:43.327', EventTime: '2018-01-24T09:43:43.327' },
+        { Id: 5, BiddingItemId: 1, ImageUrl: '', Message: 'bidder yfang submitted new bid price 2 for lanyard', CreateDate: '2018-01-24T09:43:43.343', EventTime: '2018-01-24T09:43:43.343' }
+      ];
     }
     function gotoTab(tab) {
       if (tab && tab.State) {
         vm.currentTab = angular.copy(tab);
         $state.go(tab.State);
       }
+    }
+
+    function viewItem(item) {
+      $state.go('bidding.details', { id: item.BiddingItemId });
     }
 
     //vm.test = function () {
