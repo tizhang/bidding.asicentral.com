5cd8cd96e2090c0f6b0f33f2237b652cad78f328
Caroline Chen <cchen@asicentral.com>
2018-01-24 09:27:06 -0500

    WIP MyHistory

--- Bidding.Web/App/modules/bidding/controllers/BiddingHistoryController.js
+++ Bidding.Web/App/modules/bidding/controllers/BiddingHistoryController.js
@@ -10,13 +10,10 @@
   function BiddingHistoryController($scope, $state, $filter, $q, ngTableParams) {
     var vm = this;
 
-    vm.addWatch = addWatch;
-    vm.submitBid = submitBid;
-
     init();
 
     function init() {
-      vm.tableAllLive = new ngTableParams(
+      vm.tableMyHistory = new ngTableParams(
         //{ page: 1, count: 10, filter: $scope.filter_in_grid.filter, sorting: $scope.filter_in_grid.sorting },
         {
           page: 1,
@@ -24,30 +21,26 @@
         },
         {
           total: 0,
-          getData: getAllLive
+          getData: getMyHistory
         }
       );
 
-      vm.tableAllLive.reload();
+      vm.tableMyHistory.reload();
     };
 
-    function getAllLive($defer, params) {
-      vm.liveItems = [
-        { BiddingItemId: 1, Name: 'Pen', Description: 'Blue Pen', ImageUrl: '', BidTimes: 3, Price: 3, MinNextPrice: 4, TimeLeft: '1:10:20', Expiration: '1/26/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'Live'},
-        { BiddingItemId: 2, Name: 'Mug', Description: 'Red Mug', ImageUrl: '', BidTimes: 3, Price: 5, MinNextPrice: 6, TimeLeft: '2:00:00', Expiration: '1/31/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'Live'},
-        { BiddingItemId: 3, Name: 'T-Shirt', Description: 'White T-shirt', ImageUrl: '', BidTimes: 3, Price: 10, MinNextPrice: 11, TimeLeft: '3:00:00', Expiration: '1/31/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'Live'}
+    function getMyHistory($defer, params) {
+      vm.history = [
+        { BiddingItemId: 1, Name: 'Pen', Description: 'Blue Pen', ImageUrl: '', BidTimes: 3, Price: 3, MinNextPrice: 4, TimeLeft: '1:10:20', Expiration: '1/26/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'STAG'},
+        { BiddingItemId: 2, Name: 'Mug', Description: 'Red Mug', ImageUrl: '', BidTimes: 3, Price: 5, MinNextPrice: 6, TimeLeft: '2:00:00', Expiration: '1/31/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'STAG'},
+        { BiddingItemId: 3, Name: 'T-Shirt', Description: 'White T-shirt', ImageUrl: '', BidTimes: 3, Price: 10, MinNextPrice: 11, TimeLeft: '3:00:00', Expiration: '1/31/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'STAG'}
       ];
 
-      var data = params.sorting() ? $filter('orderBy')(vm.liveItems, params.orderBy()) : vm.liveItems;
+      var data = params.sorting() ? $filter('orderBy')(vm.history, params.orderBy()) : vm.history;
       data = params.filter() ? $filter('filter')(data, params.filter()) : data;
       params.total(data.length);
       vm.totalCount = data.length;
       //vm.SelectedRows = data.slice((params.page() - 1) * params.count(), params.page() * params.count());
       $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-
-    }
-
-    function addWatch() {
 
     }
 
