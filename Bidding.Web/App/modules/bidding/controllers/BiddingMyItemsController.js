6d951c0371926f05acb5fcf6b39766defa278730
Caroline Chen <cchen@asicentral.com>
2018-01-23 22:01:27 -0500

    WIP MyItems

--- Bidding.Web/App/modules/bidding/controllers/BiddingMyItemsController.js
+++ Bidding.Web/App/modules/bidding/controllers/BiddingMyItemsController.js
@@ -5,18 +5,21 @@
     .module('bidding')
     .controller('BiddingMyItemsController', BiddingMyItemsController);
 
-  BiddingMyItemsController.$inject = ['$scope', '$state', '$filter', '$q', 'ngTableParams'];
+  BiddingMyItemsController.$inject = ['$scope', '$state', '$filter', '$q', 'ngTableParams', 'BiddingItem'];
 
-  function BiddingMyItemsController($scope, $state, $filter, $q, ngTableParams) {
+  function BiddingMyItemsController($scope, $state, $filter, $q, ngTableParams, BiddingItem) {
     var vm = this;
 
-    vm.addWatch = addWatch;
-    vm.submitBid = submitBid;
+    vm.addItem = addItem;
+    vm.editItem = editItem;
+    vm.deleteItem = deleteItem;
+    vm.sendEmail = sendEmail;
+    vm.viewItem = viewItem;
 
     init();
 
     function init() {
-      vm.tableAllLive = new ngTableParams(
+      vm.tableMyItems = new ngTableParams(
         //{ page: 1, count: 10, filter: $scope.filter_in_grid.filter, sorting: $scope.filter_in_grid.sorting },
         {
           page: 1,
@@ -24,21 +27,21 @@
         },
         {
           total: 0,
-          getData: getAllLive
+          getData: getMyItems
         }
       );
 
-      vm.tableAllLive.reload();
+      vm.tableMyItems.reload();
     };
 
-    function getAllLive($defer, params) {
-      vm.liveItems = [
-        { BiddingItemId: 1, Name: 'Pen', Description: 'Blue Pen', ImageUrl: '', BidTimes: 3, Price: 3, MinNextPrice: 4, TimeLeft: '1:10:20', Expiration: '1/26/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'Live'},
-        { BiddingItemId: 2, Name: 'Mug', Description: 'Red Mug', ImageUrl: '', BidTimes: 3, Price: 5, MinNextPrice: 6, TimeLeft: '2:00:00', Expiration: '1/31/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'Live'},
-        { BiddingItemId: 3, Name: 'T-Shirt', Description: 'White T-shirt', ImageUrl: '', BidTimes: 3, Price: 10, MinNextPrice: 11, TimeLeft: '3:00:00', Expiration: '1/31/2018', OwnerId: 1, OwnerEmail: 'tzhang@asicentral.com', Status: 'Live'}
+    function getMyItems($defer, params) {
+      vm.myItems = [
+        { BiddingItemId: 1, Name: 'Pen', Description: 'Blue Pen', ImageUrl: '', BidTimePerUser: 10, Price: 10, BiddingType: 'High Win', MinIncrement: 1, StartDate: '1/23/2018', EndDate: '2/10/2018', MinIncrement: 1, Status: 'ACTV'},
+        { BiddingItemId: 2, Name: 'Mug', Description: 'Red Mug', ImageUrl: '', BidTimePerUser: 10, Price: 15, BiddingType: 'Low Win', MinIncrement: 2, StartDate: '2/26/2018', EndDate: '2/28/2018', MinIncrement: 1, Status: 'DRAF'},
+        { BiddingItemId: 3, Name: 'T-Shirt', Description: 'White T-shirt', ImageUrl: '', BidTimePerUser: 10, Price: 10, BiddingType: 'High Win', MinIncrement: 3, StartDate: '1/28/2018', EndDate: '1/31/2018', MinIncrement: 1, Status: 'STAG'}
       ];
 
-      var data = params.sorting() ? $filter('orderBy')(vm.liveItems, params.orderBy()) : vm.liveItems;
+      var data = params.sorting() ? $filter('orderBy')(vm.myItems, params.orderBy()) : vm.myItems;
       data = params.filter() ? $filter('filter')(data, params.filter()) : data;
       params.total(data.length);
       vm.totalCount = data.length;
@@ -47,17 +50,45 @@
 
     }
 
-    function addWatch() {
+    function addItem() {
 
     }
 
-    function submitBid(bidItem) {
-      // add popup window to enter price
+    function editItem(item) {
 
-      var action = new BiddingAction(bitItem);
-      action.$save().then(function (response) { },
-        function (error) { })
     }
 
+    function deleteItem(item) {
+      var msgConfirm = '';
+
+      if (item.Status == 'DRAF') {
+        msgConfirm = 'Are you sure you want to delete this item?';
+      };
+      BootstrapDialog.confirmdirty(msgConfirm, function (result) {
+        if (result) {
+          BiddingItem.delete(item.BiddingItemId)
+            .then(
+            function (res) {
+              var result = res;
+            },
+            function (err) {
+              console.log(err);
+            });
+
+        }
+        else {
+          return;
+        }
+      });
+    }
+
+    function sendEmail(item) {
+
+    }
+
+    function viewItem(item) {
+
+    }
+    
   }
 })();
